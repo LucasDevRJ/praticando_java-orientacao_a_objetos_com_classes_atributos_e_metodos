a3c6b6d1821e394eb2b5503a1acadfbbb29eae5a
Lucas <lucaspereiradelima2020@gmail.com>
2026-06-14 10:46:49 -0300

    Método que dá percentual de aumento no salário.

--- src/com/github/lucasdevrj/principal/Funcionario.java
+++ src/com/github/lucasdevrj/principal/Funcionario.java
@@ -8,4 +8,9 @@ public class Funcionario {
     public void exibirInformacoes() {
         System.out.printf("Funcionário: %s - Cargo: %s - Salário: %.2f", nome, cargo, salario);
     }
+
+    public void reajustarSalario(double percentual) {
+        salario += salario * (percentual / 100);
+        System.out.printf("Novo salário de %s é R$ %.2f", nome, salario);
+    }
 }
--- src/com/github/lucasdevrj/principal/Principal.java
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -8,10 +8,15 @@ public class Principal {
         funcionario1.salario = 9000;
 
         funcionario1.exibirInformacoes();
+        System.out.println();
 
         Funcionario funcionario2 = new Funcionario();
         funcionario2.nome = "João";
         funcionario2.cargo = "Desenvolvedor";
         funcionario2.salario = 8500;
+
+        funcionario2.exibirInformacoes();
+        System.out.println();
+        funcionario2.reajustarSalario(5);
     }
 }
